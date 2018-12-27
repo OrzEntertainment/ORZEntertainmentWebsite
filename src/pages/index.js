7a0b8ec88becf15702cd3037279bbcded72222e3
Omar <omar2535@gmail.com>
2018-12-26 19:12:06 -0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="ORZ" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Welcome</h1>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
